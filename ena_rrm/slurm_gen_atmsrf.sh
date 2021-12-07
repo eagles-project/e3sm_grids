24e1dee1ab3903fa07e7f04d40d59c732f097f79
Christopher Jones <christopher.jones@pnnl.gov>
2021-12-07 14:25:21 -0800

    clean up slurm_gen_atmsrf.sh

--- ena_rrm/slurm_gen_atmsrf.sh
+++ ena_rrm/slurm_gen_atmsrf.sh
@@ -11,17 +11,8 @@ source .env_mach_specific.sh
 # Set full name for physics grid
 physics_grid=${atm_grid_name}
 
-# Build mkatmsrffile
 cd ${e3sm_root}/components/eam/tools/mkatmsrffile || exit 1
 
 # Run the tool from compute node
 mkatmsrffile=${e3sm_root}/components/eam/tools/mkatmsrffile/mkatmsrffile
 ${mkatmsrffile}
-
-# Convert the file to netcdf3 format:
-# if [ -e atmsrf_${physics_grid}_${date}_n4.nc ]; then
-#     ncks -3 atmsrf_${physics_grid}_${date}_n4.nc atmsrf_${physics_grid}_${date}.nc
-# fi
-
-# Exit gracefully
-# exit 0

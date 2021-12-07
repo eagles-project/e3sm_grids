6182c4fe4605e40892c56c572a7fa39254706c26
Christopher Jones <christopher.jones@pnnl.gov>
2021-12-07 08:49:55 -0800

    update config file for low-res ena test grid enax4v2

--- ena_rrm/config.sh
+++ ena_rrm/config.sh
@@ -4,10 +4,10 @@
 project=m3525
 
 # RRM grid details to specify
-dyn_grid_name=enax32v1
+dyn_grid_name=enax4v2
 atm_grid_name=${dyn_grid_name}pg2
-ocn_grid_name=oRRS18to6v3
-lnd_grid_name=r0125
+ocn_grid_name=oEC60to30v3
+lnd_grid_name=r05
 
 # directory structures to specify
 tempest_root=${HOME}/git_repos/tempestremap
@@ -20,12 +20,25 @@ mapping_root=/project/projectdirs/e3sm/mapping
 script_dir=${PWD}
 
 # homme_tool
+# ...
 
 # input scrip grid files needed for mapping (step 3)
-ocn_grid_file=${inputdata_root}/ocn/mpas-o/oRRS18to6v3/ocean.oRRS18to6v3.scrip.181106.nc
-lnd_grid_file=${inputdata_root}/lnd/clm2/mappingdata/grids/MOSART_global_8th.scrip.20180211c.nc
-ocn_scrip_file=${ocn_grid_file}
+if [ "$lnd_grid_name" == "r0125" ]; then 
+    lnd_grid_file=${inputdata_root}/lnd/clm2/mappingdata/grids/MOSART_global_8th.scrip.20180211c.nc   # r0125
+fi
+if [ "$lnd_grid_name" == "r05" ]; then
+    lnd_grid_file=${inputdata_root}/lnd/clm2/mappingdata/grids/SCRIPgrid_0.5x0.5_nomask_c110308.nc    # r05
+fi
 lnd_scrip_file=${lnd_grid_file}
+
+# ocean grid file
+if [ "$ocn_grid_name" == "oRRS18to6v3" ]; then
+    ocn_grid_file=${inputdata_root}/ocn/mpas-o/oRRS18to6v3/ocean.oRRS18to6v3.scrip.181106.nc
+fi
+if [ "$ocn_grid_name" == "oEC60to30v3" ]; then
+    ocn_grid_file=${inputdata_root}/ocn/mpas-o/oEC60to30v3/ocean.oEC60to30v3.scrip.181106.nc
+fi
+ocn_scrip_file=${ocn_grid_file}
 
 # create directories if necessary
 mkdir -p $output_root
@@ -37,7 +50,6 @@ compiler="intel"
 
 # Need an environment with at least nco and ncl; can create this with conda like:
 # > conda create --name ncl -c conda-forge nco ncl
-# module load python
-# conda activate hiccup_env
-# use e3sm_unified unless that fails :)
+
+# use e3sm_unified by default
 source /global/common/software/e3sm/anaconda_envs/load_latest_e3sm_unified_cori-knl.sh

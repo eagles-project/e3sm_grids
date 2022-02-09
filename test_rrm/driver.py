3585f424a333bdd659d8b8908dc1f82e671906fd
Christopher Jones <christopher.jones@pnnl.gov>
2022-02-09 10:53:05 -0800

    workarounds for doing imports; test_rrm driver works on cori when using ipython

--- test_rrm/driver.py
+++ test_rrm/driver.py
@@ -1,14 +1,19 @@
 from pathlib import Path
-from rrmhelper.jobmonitor import RRMJobmonitor
+import sys
+
+# add rrmhelper path
+sys.path.append(str(Path('../rrmhelper').resolve()))
+from jobmonitor import RRMJobMonitor
 
 if __name__ == "__main__":
     try:
-        script_dir = Path(__file__).resolve()
+        script_dir = Path(__file__).resolve().parent
+        print(script_dir)
     except NameError:
         script_dir = Path().cwd().resolve()
     
     config_script = 'config.sh'
-    JobMonitor = RRMJobmonitor(script_dir=script_dir,
+    JobMonitor = RRMJobMonitor(script_dir=script_dir,
                                config_script=config_script)
     JobMonitor.run(init=True, logfile=True, retry_attempts=0,
                    sleep_time=30, verbose=True, max_iter=2880)
